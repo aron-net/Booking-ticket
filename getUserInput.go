c66eeadd6889c41bc7b3231ff1df11e2b6430454
aron-helu <aronmilahela@gmail.com>
2023-07-07 19:53:29 +0300

    Add get user input

---
+++ getUserInput.go
@@ -0,0 +1,26 @@
+package main
+
+
+import "fmt"
+
+func getUserInput() (string, string, string, uint) {
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	// ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("How many tickets do you want: ")
+	fmt.Scan(&userTickets)
+
+	return firstName, lastName, email, userTickets
+}
